a35282f2d10cde4be26bdae0a8c211ec5cdad2a7
VinayV9 <vinayveeren@gmail.com>
2018-04-21 22:52:24 +0530

    updated for mysql db connection configd

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,8 @@
 module.exports  = {
     secretKey : "superSecrete",
-    mongoUri : "mongodb://localhost:27017/auth-app"
+    mongoUri : "mongodb://localhost:27017/auth-app",
+    mysqlhost:"127.0.0.1",
+    mysqluser:"root",
+    mysqlpassword:"root",
+    mysqldb:"authDemo"
 }
